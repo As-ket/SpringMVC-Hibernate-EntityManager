2cad23475df9a6b5f79a9ee6018c8a3626c99499
Max <Asket-axe@mail.ru>
2023-03-26 17:37:30 +0300

    правки 1.0

--- src/main/java/service/CarServiceImpl.java
+++ src/main/java/service/CarServiceImpl.java
@@ -8,13 +8,13 @@ import java.util.List;
 public class CarServiceImpl implements CarService {
 
     private final CarDao carDao = new CarDaoImpl();
+    private final List<Car> carInWeb = carDao.getCars();
 
 
     @Override
     public List<Car> getCars(int count) {
-        List<Car> carInWeb = carDao.getCars();
-        if (count <= 5 && count > 0) {
-            return carDao.getCars().subList(0, count);
+        if (count < 5 && count > 0) {
+            return carInWeb.subList(0, count);
         } else if (count <= 0) {
             return null;
         }
